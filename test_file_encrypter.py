719a2e5112ee337e712c819b7b116e59679c5d08
Rohan-J-S <53927306+Rohan-J-S@users.noreply.github.com>
2020-11-10 18:27:03 +0530

    Update test_file_encrypter.py

--- test_file_encrypter.py
+++ test_file_encrypter.py
@@ -1,7 +1,6 @@
 text_file = input("enter file to be encrypted: ")
 x = open(text_file,"r",)
 text = x.readlines()
-
 key = {'a':'h','b':'d','c':'s','d':'n','e':'a','f':'i','g':'q','h':'k','i':'e','j':'y','k':'u','l':'j','m':'o','n':'t','o':'c','p':'w','q':'w','r':'b','s':'r','t':'f','u':'l','v':'x','w':'p','x':'g','y':'z','z':'v',' ':' '}
 for y in range(len(text)):
     s = ""
@@ -10,8 +9,6 @@ for y in range(len(text)):
             break
         s += key[text[y][z]]
     text[y] = s
-
-
 x.close()
 y = open(text_file,"w+")
 for z in text:
